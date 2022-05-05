f6a7b8bec53068ca66b61d11a9c37e31e99e279a
Yerson Reyes <yerson.programador@gmail.com>
2022-05-04 21:27:00 -0400

    proyecto terminado

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="text-center mt-3">Listado de tareas</h1>
+    <div class="container mt-5 mb-5">
+      <div class="input-group m-0">
+        <input
+          v-model="nuevaTarea"
+          v-on:keypress.enter="agregarNuevaTarea"
+          type="text"
+          class="form-control"
+          placeholder="Nueva tarea"
+        />
+        <div class="input-group-append">
+          <button
+            @click="agregarNuevaTarea"
+            class="btn btn-outline-secondary"
+            type="button"
+            id="button-addon2"
+          >
+            Button
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="container section2">
+      <ul class="list-group">
+        <Tarea
+          v-for="(tarea, $index) in tareas"
+          :key="$index"
+          :nombreTarea="tarea"
+          @myEvent="eliminarTarea($index)"
+        />
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tarea from "./components/Tarea";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Tarea },
+  data: () => ({
+    nuevaTarea: "",
+    tareas: [],
+  }),
+  methods: {
+    agregarNuevaTarea() {
+      this.tareas.unshift(this.nuevaTarea);
+      this.nuevaTarea = "";
+    },
+
+    eliminarTarea(eliminar) {
+      if (eliminar == 0) {
+        this.tareas.shift();
+      } else {
+        this.tareas.splice(eliminar, eliminar);
+      }
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.section2 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+.section2 ul {
+  width: 100%;
+}
+.list-group-item {
+  display: flex;
 }
 </style>
---
+++ src/components/Tarea.vue
@@ -0,0 +1,20 @@
+<template>
+  <li class="list-group-item">
+    {{ nombreTarea }}
+
+    <button @click="eliminar" class="btn btn-danger ml-auto">eliminar</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["nombreTarea"],
+  methods: {
+    eliminar() {
+      this.$emit("myEvent", null);
+    },
+  },
+};
+</script>
+
+<style></style>
